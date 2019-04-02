a5b50b37a1766f1ee0247089eaee7e4d2b5fcbcf
lromeraj <lromerajdev@gmail.com>
2019-04-02 23:02:53 +0200

    updated doc

---
+++ docs/html/search/enums_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['cardinalpoint',['CardinalPoint',['../types_8h.html#a7a09f2d4d51c4b4ba7957bbe676a3855',1,'types.h']]],
+  ['color',['Color',['../ui_8h.html#ab87bacfdad76e61b9412d7124be44c1c',1,'ui.h']]]
+];
---
+++ docs/html/search/enumvalues_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['left',['LEFT',['../cmd_8h.html#a00a332e55439547f3ed90d0839bd1995adb45120aafd37a973140edee24708065',1,'cmd.h']]]
+];
